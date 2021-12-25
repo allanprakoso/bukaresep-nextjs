4ae34a1ae31543c3d880fa24b810ea46f82e07cb
caroline2697 <caroline.2697@students.amikom.ac.id>
2021-12-25 17:46:06 +0700

    footer

--- src/components/test/footer.js
+++ src/components/test/footer.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <footer className="bg-gray-50 py-5 md:py-8 lg:pt-14 lg:pb-10">
@@ -5,30 +7,26 @@ const Footer = () => {
         <img src="logo/logo-identity.svg" className="h-8 md:h-[3.125rem] " />
       </div>
       <div className="flex justify-center my-4 md:my-6 lg:my-10">
-        <a
-          href="#"
-          className="mx-3 md:mx-5 text-caption md:text-base font-medium uppercase text-gray-600"
-        >
-          Resep
-        </a>
-        <a
-          href="#"
-          className="mx-3 md:mx-5 text-caption md:text-base font-medium uppercase text-gray-600"
-        >
-          Artikel
-        </a>
-        <a
-          href="#"
-          className="mx-3 md:mx-5 text-caption md:text-base font-medium uppercase text-gray-600"
-        >
-          Tentang kami
-        </a>
-        <a
-          href="#"
-          className="mx-3 md:mx-5 text-caption md:text-base font-medium uppercase text-gray-600"
-        >
-          Kontak
-        </a>
+        <Link href={"#"}>
+          <a className="mx-3 md:mx-5 text-caption md:text-base font-medium uppercase text-gray-600">
+            Resep
+          </a>
+        </Link>
+        <Link href={"#"}>
+          <a className="mx-3 md:mx-5 text-caption md:text-base font-medium uppercase text-gray-600">
+            Artikel
+          </a>
+        </Link>
+        <Link href={"#"}>
+          <a className="mx-3 md:mx-5 text-caption md:text-base font-medium uppercase text-gray-600">
+            Tentang kami
+          </a>
+        </Link>
+        <Link href={"#"}>
+          <a className="mx-3 md:mx-5 text-caption md:text-base font-medium uppercase text-gray-600">
+            Kontak
+          </a>
+        </Link>
       </div>
       <div className="flex justify-center border-t-[1px] border-gray-200">
         <p className="text-gray-400 text-tiny my-4 md:my-6 lg:my-10">
