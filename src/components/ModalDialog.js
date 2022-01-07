4eccb64332ad17c9f77b196cc4af0567af232fba
caroline2697 <caroline.2697@students.amikom.ac.id>
2022-01-07 09:50:22 +0700

    disableScroll

--- src/components/ModalDialog.js
+++ src/components/ModalDialog.js
@@ -2,34 +2,37 @@ import Button from "./Button";
 import { Crossl } from "../assets/icons";
 import disableScroll from "disable-scroll";
 
-const Modal = ({ children, closeModal }) => {
+const Modal = ({ children, close }) => {
   return (
     // wrapper & bg
-    <div className="modalDialog absolute inset-0 z-50">
-      <div className="modalbg bg-gray-400/30 h-[100vh] fixed inset-0 flex justify-center items-center overflow-y:hidden">
-        <div className="modalbody relative bg-white rounded-xl w-[587px] ">
-          <div id="closeModal" className="flex justify-end mt-4 mx-2">
-            <Button
-              type="button"
-              color="LINK"
-              onClick={() => closeModal(false, disableScroll.off())}
-            >
-              <svg
-                width="24px"
-                height="24px"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
+    disableScroll.on(),
+    (
+      <div className="modalDialog absolute inset-0 z-50">
+        <div className="modalbg bg-gray-400/30 h-[100vh] fixed inset-0 flex justify-center items-center overflow-y:hidden">
+          <div className="modalbody relative bg-white rounded-xl w-[587px] ">
+            <div id="closeModal" className="flex justify-end mt-4 mx-2">
+              <Button
+                type="button"
+                color="LINK"
+                onClick={() => close(false, disableScroll.off())}
               >
-                <Crossl />
-              </svg>
-            </Button>
-          </div>
-          <div id="modalContent" className="mx-12 flex flex-col pb-8">
-            {children}
+                <svg
+                  width="24px"
+                  height="24px"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <Crossl />
+                </svg>
+              </Button>
+            </div>
+            <div id="modalContent" className="mx-12 flex flex-col pb-8">
+              {children}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 
