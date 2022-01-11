fdc8b1867cd61d39c08293294f31034ab8c0be14
caroline2697 <caroline.2697@students.amikom.ac.id>
2022-01-12 01:38:25 +0700

    index user, add medium font in global.css

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,26 +1,68 @@
-import CarouselRecipe from "../../parts/CarouselRecipe";
-import RecipeSlider from "../../parts/RecipeSlider";
-import { Modal, ModalTitle, ModalContent } from "../../components/ModalDialog";
-import { useState } from "react";
+import { Search } from "../../assets/icons";
+import RecipeSlider from "../../parts/global/RecipeSlider"
+import CarouselRecipe from "../../parts/global/CarouselRecipe"
+import GridListRecipe from "../../parts/global/GridListRecipe"
+import ContainerXL from "../../components/ContainerXL"
 import Button from "../../components/Button";
-import disableScroll from "disable-scroll";
-import DeleteCollection from "../../parts/Creator/DeleteCollection";
-import EditCollection from "../../parts/Creator/EditCollection";
-import AddRecipetoCollection from "../../parts/Creator/AddRecipetoCollection";
-
-function Home() {
-  const [openAtribute, setOpenAtribute] = useState(true);
-
+const Home = () => {
   return (
-    <div className="flex flex-col space-y-20 mt-[120px] mx-[360px]">
-      <Button color="PRIMARY" onClick={() => setOpenAtribute(true)}>
-        Open Modal
-      </Button>
+    <>
+      <section className="header">
+        <div className="bg-[url('/pic/userHeader.jpg')] bg-cover rounded-xl flex flex-col items-center space-y-6 pt-48 pb-20">
+          <h1 className="text-display font-quicksand font-bold text-gray-800">Temukan resep masakan</h1>
+          <p className="text-h5 font-quicksand font-medium ">Kumpulan beragam resep masakan lezat untuk makan sehari-hari</p>
+        </div>
 
-      {openAtribute && (
-        <AddRecipetoCollection close={() => setOpenAtribute(false)} />
-      )}
-    </div>
+        <div className="flex justify-center -mt-8 drop-shadow-md">
+          <form className="flex items-center ml-8 bg-white py-4 px-5 rounded-lg">
+            <svg
+              width="16px"
+              height="16px"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              className="mr-3"
+            >
+              <Search />
+            </svg>
+            <input id="search" type="text" placeholder="Cari resep"
+              className="focus:outline-none text-base text-gray-600 w-[520px]"
+            />
+          </form>
+        </div>
+        <div className="flex space-x-6 justify-center mt-5">
+          <p className="text-base font-medium text-gray-600 ">Direkomendasikan: </p>
+          <a className="text-base text-brand-dark cursor-pointer hover:underline">ayam </a>
+          <a className="text-base text-brand-dark cursor-pointer hover:underline">kangkung </a>
+          <a className="text-base text-brand-dark cursor-pointer hover:underline">soto </a>
+          <a className="text-base text-brand-dark cursor-pointer hover:underline">rendang </a>
+          <a className="text-base text-brand-dark cursor-pointer hover:underline">bakso </a>
+        </div>
+      </section>
+      <ContainerXL>
+        <section className="resep-terbaru mt-32">
+          <h4 className="text-h4 font-quicksand font-bold text-gray-800 mb-5">Resep Terbaru</h4>
+          <RecipeSlider />
+        </section>
+
+        <section className="resep-carousel mt-28">
+          <CarouselRecipe />
+        </section>
+
+        <section className="grid-recipe">
+          {/* <GridListRecipe /> */}
+        </section>
+
+        <section className="banner mt-32">
+          <div className="bg-[url('/pic/bannerUser.jpg')] bg-cover rounded-xl flex flex-col pt-20 pb-16 px-20">
+            <h3 className="text-h3 font-quicksand font-bold text-white mb-2">Yuk, bagikan resepmu</h3>
+            <p className="text-lg font-quicksand font-medium text-white">Jadilah penulis resep dan dapatkan penghasilan dari resep masakanmu.</p>
+            <div className="mt-10">
+              <Button>Bergabung Sekarang</Button>
+            </div>
+          </div>
+        </section>
+      </ContainerXL>
+    </>
   );
 }
 
